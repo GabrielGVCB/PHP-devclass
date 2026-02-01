a5700dfe011a79707e43d82aca36fe2504fed193
GabrielGVCB <gabriel.vinicius06.gb@gmail.com>
2026-02-01 00:39:36 -0300

    feat: Adicionando pages de contato.php

---
+++ app/functions/validate.php
@@ -0,0 +1,3 @@
+<?php
+
+function validate(array $fields) {}
---
+++ public/pages/forms/contato.php
@@ -0,0 +1,10 @@
+<?php
+
+require "../../../app/functions/custom.php";
+
+dd($_POST);
+
+// $validate = validate([
+//     'name' => 'n',
+//     'email' => 's',
+// ]);
