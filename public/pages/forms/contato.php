3bd50422574d6f61e95d0694ae3cfe797ef1daa9
GabrielGVCB <gabriel.vinicius06.gb@gmail.com>
2026-02-05 22:18:06 -0300

    feat: Adicionando flash message e func isEmpty

--- public/pages/forms/contato.php
+++ public/pages/forms/contato.php
@@ -2,6 +2,13 @@
 
 require "../../../bootstrap.php";
 
+if (isEmpty()) {
+    flash('message', 'Preencha todos os campos!');
+
+    header("location:/?page=contato");
+    exit;
+}
+
 $validate = validate([
     'name' => 's',
     'email' => 'e',
